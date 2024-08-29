07d7be6796ece3bf4ead1d51b77cb46717e832eb
dona6382 <46666529+dona6382@users.noreply.github.com>
2024-08-29 16:37:31 +0900

    [dataServer] interface 수정

--- data-server/src/type/product-review.ts
+++ data-server/src/type/product-review.ts
@@ -1,6 +1,5 @@
 export interface ProductReview {
     reviewer: string;       // 리뷰 작성자
     rating: number;         // 평점 (1~5)
-    comment: string;        // 리뷰 내용
     date: string;           // 작성 날짜 (형식: yyyy-MM-dd)
 }
